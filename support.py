5c219e55cc0aec2147c3a5b7ba507fc21fa2a0a3
mmandarinn <ro7578479@gmail.com>
2025-07-01 23:25:08 +0300

    "Добавлена функция бана и разбана"

--- support.py
+++ support.py
@@ -6,14 +6,15 @@ import telebot
 bot = telebot.TeleBot('TOKEN',skip_pending=True)
 
 gr_id = str('')
-last_time_dic = {}
+last_time_dic = {} #словарь для сохранения времени взаимодействия
+ban_list = []
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id,'Здравствуйте! Я бот для тех. поддержки команды SIGN, для обращения в поддержку подробно опишите проблему, возникшую в процессе работы с ботом, одним сообщением, приложив скриншот. Учтите, минимальное время между сообщениями в поддержку составляет 6 часов, относитесь с умом к вашим запросам. Постараемся ответить в кратчайшие сроки!')
     print(message.chat.id,'     ', message.from_user.username)
     last_time_dic[message.chat.id] = datetime.now()+timedelta(hours=-6)
-#Продумать бан мут
+
 @bot.message_handler(commands=['answer'])
 def sup_ans(message):
     if gr_id in str(message.chat.id):
@@ -23,18 +24,33 @@ def sup_ans(message):
         except:
             bot.reply_to(message,'Произошла какая-то ошибка, проверьте что вы ответили на сообщение с указанием айди и написали /answer {ответ}')
 
+@bot.message_handler(commands=['ban'])
+def ban(message):
+    if str(message.chat.id) == gr_id:
+        ban_list.append(message.reply_to_message.text[13:])
+        bot.send_message(message.chat.id, 'Забанен')
+
+@bot.message_handler(commands=['unban'])
+def ban(message):
+    if str(message.chat.id) == gr_id:
+        ban_list.remove(message.reply_to_message.text[13:])
+        bot.send_message(message.chat.id, 'Разбанен')
+
 @bot.message_handler(func=lambda message:True)
 def sup(message):
-    if not(gr_id in str(message.chat.id)):
-        us_id = message.chat.id
-        delta = datetime.now() - last_time_dic[message.chat.id]
-        if delta.seconds >= 21600:
-            bot.reply_to(message, 'Ваше сообщение передано в тех. поддержу, ожидайте ответа')
-            bot.send_message(chat_id=gr_id, text=f'Сообщение от {us_id}:')
-            bot.forward_message(gr_id,us_id,message.id)
-            last_time_dic[message.chat.id] = datetime.now()
-        else:
-            bot.send_message(message.chat.id, 'Время между обращениями не может составлять менее 6 часов')
+    if str(message.chat.id) in ban_list:
+        bot.send_message(message.chat.id, 'Отказано в доступе')
+    else:
+        if not(gr_id in str(message.chat.id)):
+            us_id = message.chat.id
+            delta = datetime.now() - last_time_dic[message.chat.id]
+            if delta.seconds >= 21600:
+                bot.reply_to(message, 'Ваше сообщение передано в тех. поддержу, ожидайте ответа')
+                bot.send_message(chat_id=gr_id, text=f'Сообщение от {us_id}')
+                bot.forward_message(gr_id,us_id,message.id)
+                last_time_dic[message.chat.id] = datetime.now()
+            else:
+                bot.send_message(message.chat.id, 'Время между обращениями не может составлять менее 6 часов')
 
 
 
